f387873aece3d1a9b6942d42fae9919e8108cc20
goast <goastie@goast.ai>
2023-10-19 22:41:49 +0000

    Fix for issue

--- src/DisplayUser.js
+++ src/DisplayUser.js
@@ -4,12 +4,23 @@ function DisplayUser({ user }) {
   const [username, setUsername] = useState({first: 'John', last: 'Smith'});
 
   return (
-    <>
-      <div>Firstname: {username.first}</div>
-      <div>Lastname: {username.last}</div>
-      <button onClick={() => setUsername(null)}>Clear Name</button>
-    </>
-  );
+  if (username === null) {
+    return (
+      <>
+        <div>No user selected</div>
+        <button onClick={() => setUsername({first: 'John', last: 'Smith'})}>Reset Name</button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>Firstname: {username.first}</div>
+        <div>Lastname: {username.last}</div>
+        <button onClick={() => setUsername(null)}>Clear Name</button>
+      </>
+    );
+  }
 }
 
 export default DisplayUser;
+
